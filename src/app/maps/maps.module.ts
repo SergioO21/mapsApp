4673000abd11af12c2d96b4b78356a3ab09f3f04
Sergio Orejarena <sergioorueda@gmail.com>
2024-11-07 09:54:59 -0500

    Implement mapbox

--- src/app/maps/maps.module.ts
+++ src/app/maps/maps.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 
+import mapboxgl from "mapbox-gl";
+mapboxgl.accessToken =
+	"pk.eyJ1Ijoic2VyZ2lvbzIxIiwiYSI6ImNtMmoyczRwbjAxNmUya29vbHZ4NWNkajMifQ.tyMb0E3fg0G3mq2tDZ2T1A";
+
 import { MapsRoutingModule } from "./maps-routing.module";
 import { MiniMapComponent, SideMenuComponent } from "./components";
 import { MapsLayoutComponent } from "./layout";
