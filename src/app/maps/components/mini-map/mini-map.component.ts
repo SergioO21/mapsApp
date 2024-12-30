5c34f15c74dd2ad2bcfbe18c22c784f825682411
Sergio Orejarena <sergioorueda@gmail.com>
2024-12-29 21:43:20 -0500

    Properties page

--- src/app/maps/components/mini-map/mini-map.component.ts
+++ src/app/maps/components/mini-map/mini-map.component.ts
@@ -1,8 +1,39 @@
-import { Component } from "@angular/core";
+import {
+	AfterViewInit,
+	Component,
+	ElementRef,
+	Input,
+	ViewChild,
+} from "@angular/core";
+import { Map, Marker } from "mapbox-gl";
 
 @Component({
 	selector: "app-mini-map",
 	templateUrl: "./mini-map.component.html",
 	styleUrl: "./mini-map.component.css",
 })
-export class MiniMapComponent {}
+export class MiniMapComponent implements AfterViewInit {
+	@Input() lngLat?: [number, number];
+	@ViewChild("map") divMap?: ElementRef;
+
+	map?: Map;
+
+	ngAfterViewInit(): void {
+		if (!this.divMap) throw "HTML document not found";
+		if (!this.lngLat) throw "LngLat is required";
+
+		this.map = new Map({
+			container: this.divMap.nativeElement,
+			style: "mapbox://styles/mapbox/streets-v12",
+			center: this.lngLat,
+			zoom: 15,
+			interactive: false,
+		});
+
+		const color = "#xxxxxx".replace(/x/g, () =>
+			((Math.random() * 16) | 0).toString(16)
+		);
+
+		new Marker({ color }).setLngLat(this.lngLat).addTo(this.map);
+	}
+}
--- src/app/maps/pages/properties-page/properties-page.component.ts
+++ src/app/maps/pages/properties-page/properties-page.component.ts
@@ -1,7 +1,33 @@
 import { Component } from "@angular/core";
+import { House } from "./interfaces";
 
 @Component({
 	templateUrl: "./properties-page.component.html",
 	styleUrl: "./properties-page.component.css",
 })
-export class PropertiesPageComponent {}
+export class PropertiesPageComponent {
+	houses: House[] = [
+		{
+			title: "Residential House, Canada",
+			description: "Beautiful property in Katana, Canada",
+			lngLat: [-75.92722289474008, 45.280015511264466],
+		},
+		{
+			title: "Beach House, Mexico",
+			description: "Beautiful beach house in Acapulco, Mexico",
+			lngLat: [-99.91287720907991, 16.828940930185748],
+		},
+		{
+			title: "Apartment, Argentina",
+			description:
+				"Luxurious apartment in the heart of Buenos Aires, Argentina",
+			lngLat: [-58.430166677283445, -34.57150108832866],
+		},
+		{
+			title: "Commercial Space, Spain",
+			description:
+				"Commercial space available in Madrid, Spain, near El Jardín Secreto.",
+			lngLat: [-3.7112735618380177, 40.42567285425766],
+		},
+	];
+}
